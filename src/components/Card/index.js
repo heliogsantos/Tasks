f21b9da7d6ca66cc41d02ab2468129191037766a
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-08-26 21:16:37 -0300

    criei o menu de ação do card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BsTextRight } from 'react-icons/bs';
 
-import { CardWraper, Title, CardBody } from './style'
+import { CardWraper, Title, CardBody, Menu, List } from './style'
 import Button from '../Button';
 
 const Card = props => {
+
+  const [menuActive, setMenuActive] = useState(false)
+  
+  const handleMenu = () => setMenuActive(menu => !menu)
+
   return (
     <CardWraper style={{background: props.labelColor}}>
       <Title>Titulo</Title>
       <CardBody>
         <p>Descrição card</p>
-        <Button
-          padding="0"
-          color="black"
-          text={!props.done ? <BsTextRight /> : false}>
-        </Button>
+        {
+          !props.done ? <BsTextRight onClick={handleMenu} /> : false}
+        {
+          menuActive ? 
+            <Menu>
+              <List onClick={handleMenu} color="cancel">remover</List>
+              <List onClick={handleMenu} color="primary">Em andamento</List>
+              <List onClick={handleMenu}  color="blue-default">pronto</List>
+            </Menu> 
+          : false
+        }
       </CardBody>
     </CardWraper>
   )
--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -15,7 +15,6 @@ export const Title = styled.h2 `
   font-weight: 400;
   font-size: 16px;
 `
-
 export const CardBody = styled.div `
   width: 100%;
   display: flex;
@@ -31,5 +30,64 @@ export const CardBody = styled.div `
     border-style: none;
     background: transparent;
     cursor: pointer;
+  }
+
+  &:last-child {
+    cursor: pointer;
+  }
+`
+
+export const Menu = styled.ul `
+  position: absolute;
+  right: 0;
+  bottom: -120px;
+  width: 200px;
+  height: 120px;
+  background: #ffffff;
+  z-index: 2;
+  padding: 21px 20px;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-shadow: 2px -8px 105px -18px rgba(0,0,0,.19);
+  -moz-box-shadow: 2px -8px 105px -18px rgba(0,0,0,.19);
+  box-shadow: 2px -8px 105px -18px rgba(0,0,0,.19);
+  align-items: flex-start;
+
+  &:before {
+    content: '';
+    width: 17px;
+    height: 17px;
+    background: #ffffff;
+    position: absolute;
+    right: 17px;
+    top: -7px;
+    transform: rotate(46deg);
+
+  }
+`
+
+export const List = styled.li `
+  cursor: pointer;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 2;
+  position: relative;
+  margin-left: 20px;
+  transition: .2s linear;
+  color: ${ props => `var(--${props.color})`};
+
+  &:before {
+    content: '';
+    width: 15px;
+    height: 15px;
+    background: ${ props => `var(--${props.color})`};
+    position: absolute;
+    left: -24px;
+    top: 6px;
+    border-radius: 10%;
+  }
+
+  &:hover {
+    opacity: .7;
   }
 `
